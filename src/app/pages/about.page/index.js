63de05ade16f50327c5f9160d9c4ee40d0c06e51
mrjonas <jonas.l.antanaitis@gmail.com>
2018-01-05 18:18:37 +0200

    IE image fix 10

--- src/app/pages/about.page/index.js
+++ src/app/pages/about.page/index.js
@@ -5,25 +5,27 @@ class AboutPage extends React.Component {
 
     render() {
         return (
-            <div className="container" style={{minHeight: 'calc(100vh - 200px)'}}>
-                <div>
-                    <div className="row">
-                        <div className="col-xs-12">
-                            <div className="card text-center">
-                                <h4 className="text-left">Autoriai</h4>
-                                <div>
-                                    <img src="/img/autoriai.jpg" alt="Dviračių maršrutų autoriai"
-                                         className="my-4 img-authors"/>
+            <div>
+                <div className="container">
+                    <div>
+                        <div className="row">
+                            <div className="col-xs-12">
+                                <div className="card text-center">
+                                    <h4 className="text-left">Autoriai</h4>
+                                    <div>
+                                        <img src="/img/autoriai.jpg" alt="Dviračių maršrutų autoriai"
+                                             className="my-4 img-authors"/>
+                                    </div>
+                                    <p className="lead">Mes esame Ieva ir Jonas. Gražiu oru mėgstame dviračiais
+                                        važinėtis po Lietuvą.</p>
+                                    <p className="lead">Šiame bloge rasite visus mūsų išbandytus maršrutus.</p>
+                                    <p className="lead">
+                                        Mes esame ne dviračių sporto, o labiau poilsio dviračiais entuziastai.
+                                        Todėl siūlomi maršrutai yra skirti pradedantiesiems arba vidutiniokams.
+                                        Tačiau idėjų savo žygiams pasisems ir labiau patyrę.
+                                    </p>
+                                    <p className="lead">Linkime smagių kelionių!</p>
                                 </div>
-                                <p className="lead">Mes esame Ieva ir Jonas. Gražiu oru mėgstame dviračiais
-                                    važinėtis po Lietuvą.</p>
-                                <p className="lead">Šiame bloge rasite visus mūsų išbandytus maršrutus.</p>
-                                <p className="lead">
-                                    Mes esame ne dviračių sporto, o labiau poilsio dviračiais entuziastai.
-                                    Todėl siūlomi maršrutai yra skirti pradedantiesiems arba vidutiniokams.
-                                    Tačiau idėjų savo žygiams pasisems ir labiau patyrę.
-                                </p>
-                                <p className="lead">Linkime smagių kelionių!</p>
                             </div>
                         </div>
                     </div>
