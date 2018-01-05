21dde8d315f804cefb23d75ac60621b7049cbd66
mrjonas <jonas.l.antanaitis@gmail.com>
2018-01-05 18:05:23 +0200

    IE image fix 9

--- src/app/pages/about.page/index.js
+++ src/app/pages/about.page/index.js
@@ -5,17 +5,18 @@ class AboutPage extends React.Component {
 
     render() {
         return (
-            <div>
-                <div className="container" style={{minHeight: 'calc(100vh - 200px)'}} >
-                    <div>
+            <div className="container" style={{minHeight: 'calc(100vh - 200px)'}}>
+                <div>
                     <div className="row">
                         <div className="col-xs-12">
                             <div className="card text-center">
                                 <h4 className="text-left">Autoriai</h4>
                                 <div>
-                                <img src="/img/autoriai.jpg" alt="Dviračių maršrutų autoriai" className="my-4 img-authors"/>
+                                    <img src="/img/autoriai.jpg" alt="Dviračių maršrutų autoriai"
+                                         className="my-4 img-authors"/>
                                 </div>
-                                <p className="lead">Mes esame Ieva ir Jonas. Gražiu oru mėgstame dviračiais važinėtis po Lietuvą.</p>
+                                <p className="lead">Mes esame Ieva ir Jonas. Gražiu oru mėgstame dviračiais
+                                    važinėtis po Lietuvą.</p>
                                 <p className="lead">Šiame bloge rasite visus mūsų išbandytus maršrutus.</p>
                                 <p className="lead">
                                     Mes esame ne dviračių sporto, o labiau poilsio dviračiais entuziastai.
@@ -27,8 +28,7 @@ class AboutPage extends React.Component {
                         </div>
                     </div>
                 </div>
-                    <div>
-                {/*<Footer/>*/}
+                <Footer/>
             </div>
         );
     }
