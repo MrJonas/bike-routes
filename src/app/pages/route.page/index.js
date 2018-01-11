b9e90111ef141ef54d7a2233dc893ee2bb63e24b
mrjonas <jonas.l.antanaitis@gmail.com>
2018-01-11 22:05:07 +0200

    testing rendering 5

--- src/app/pages/route.page/index.js
+++ src/app/pages/route.page/index.js
@@ -5,11 +5,36 @@ import Footer from './../../components/footer';
 import LazyLoad from 'react-lazyload';
 import Gallery from 'react-grid-gallery';
 
+const IMAGES =
+    [{
+        src: "https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_b.jpg",
+        thumbnail: "https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_n.jpg",
+        thumbnailWidth: 320,
+        thumbnailHeight: 174,
+        isSelected: true,
+        caption: "After Rain (Jeshu John - designerspics.com)"
+    },
+        {
+            src: "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_b.jpg",
+            thumbnail: "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_n.jpg",
+            thumbnailWidth: 320,
+            thumbnailHeight: 212,
+            tags: [{value: "Ocean", title: "Ocean"}, {value: "People", title: "People"}],
+            caption: "Boats (Jeshu John - designerspics.com)"
+        },
+
+        {
+            src: "https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_b.jpg",
+            thumbnail: "https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_n.jpg",
+            thumbnailWidth: 320,
+            thumbnailHeight: 212
+        }]
+
 class RoutePage extends React.Component {
 
     constructor() {
         super();
-        this.state = {lightboxIsOpen: false, currentImage: 0, images: [], route: null};
+        this.state = {images: [], route: null};
     }
 
     setRoute(route) {
@@ -92,7 +117,7 @@ class RoutePage extends React.Component {
                             <Card className="mb-2">
                                 <CardBlock >
                                     <CardTitle className="text-left">Galerija</CardTitle>
-                                    <Gallery images={this.state.images} maxRows="3"/>
+                                    <Gallery images={IMAGES} maxRows="3"/>
                                 </CardBlock>
                             </Card>
                         </div>
