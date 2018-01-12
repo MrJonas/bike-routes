6210fadb258f99cb29dddad8058360f19bc16706
mrjonas <jonas.l.antanaitis@gmail.com>
2018-01-12 18:28:35 +0200

    testing rendering ą0

--- app.dev.js
+++ app.dev.js
@@ -2,4 +2,5 @@ require('babel-register');
 require("babel-core").transform("code", {
     plugins: ["transform-object-assign"]
 });
+require('babel-plugin-transform-object-assign');
 require('./build_scripts/devServer');
